c473bd5e7bc3cc3af87a657f6df7fd5c178569c6
Ryan Clark <clark.ryan@gmail.com>
2024-11-02 15:47:09 -0400

    scroll to top

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior() {
+    // always scroll to top
+    return { top: 0 }
+  },
   routes: [
     {
       path: '/',
